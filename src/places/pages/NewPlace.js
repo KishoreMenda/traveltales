7962748e966e647ccfdf6f37b1d66af3c22fd462
KishoreMenda <m.kishore0309@gmail.com>
2024-05-27 18:50:23 -0700

    App AuthContext

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,71 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./PlaceForm.css";
+import { useForm } from "../../shared/hooks/form-hook";
+
 const NewPlace = () => {
-  return (
-    <div>NewPlace</div>
+  //using my custom hook
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        valid: "",
+        isValid: false,
+      },
+    },
+    false
   );
-}
+
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title !"
+        onInput={inputHandler}
+      ></Input>
+      <Input
+        id="description"
+        element="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters) !"
+        onInput={inputHandler}
+      ></Input>
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address!"
+        onInput={inputHandler}
+      ></Input>
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </form>
+  );
+};
 
 export default NewPlace;
